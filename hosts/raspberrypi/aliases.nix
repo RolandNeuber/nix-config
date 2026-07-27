a6731e22b23180dc8a64bf07053a5456209531b2
Roland Neuber <rolandmartinneuber@gmail.com>
2026-07-27 10:42:59 +0200

    Add raspberrypi as proper host.

---
+++ hosts/raspberrypi/aliases.nix
@@ -0,0 +1,10 @@
+{ ... }:
+let
+  flakeLocation = "--flake ~/nix-config/hosts/raspberrypi";
+in {
+  home.shellAliases = rec {
+    ll = "ls -al";
+    switch = "home-manager switch " + flakeLocation;
+    update = "nix flake update " + flakeLocation + " && " + switch;
+  };
+}
