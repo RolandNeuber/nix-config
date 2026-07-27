d25d6f61b5ff48d7007b4d810d5936f09b5d6b94
Roland Neuber <rolandmartinneuber@gmail.com>
2026-07-27 10:42:59 +0200

    Add raspberrypi as proper host.

---
+++ hosts/raspberrypi/home.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "roland";
+  home.homeDirectory = "/home/roland";
+  
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "26.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  targets.genericLinux.gpu.enable = true;
+
+  imports = [
+    ./aliases.nix
+    ../../modules/home/zsh.nix
+    ../../modules/home/tailscale.nix
+  ];
+}
+
